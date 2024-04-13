05d9b3f7e5629ebf0b000bb7e2671f2a6fcfa7d9
André Luiz L <144447401+Andre-Luiz-lopes@users.noreply.github.com>
2024-04-13 10:29:43 -0300

    Update Principal.java

--- CAP-05/Q1R/src/br/edu/principal/Principal.java
+++ CAP-05/Q1R/src/br/edu/principal/Principal.java
@@ -7,7 +7,7 @@ public class Principal {
         Scanner input = new Scanner(System.in);
         int anoAtual;
         double salario = 1000.0;
-        double percentual = 0.015; // 1.5%
+        double percentual = 0.015; 
 
         System.out.print("Digite o ano atual: ");
         anoAtual = input.nextInt();
@@ -15,13 +15,13 @@ public class Principal {
         double novoSalario = calcularSalario(anoAtual, salario, percentual);
         System.out.println("Salário atual em " + anoAtual + ": R$" + novoSalario);
 
-        input.close();
+        
     }
 
     public static double calcularSalario(int anoAtual, double salario, double percentual) {
         for (int ano = 2006; ano <= anoAtual; ano++) {
             salario += salario * percentual;
-            percentual *= 2; // Aumento dobro do percentual
+            percentual *= 2; 
         }
         return salario;
     }
